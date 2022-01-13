feb5d1edfc9158d36afb6b4fb7a28144de54f3af
Angelia Widjaja <angeliawidjajaa@gmail.com>
2022-01-13 15:58:00 +0700

    feat: Add Splash Screen + Base Home Activity

---
+++ app/src/main/java/com/example/pathway_jogging/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.pathway_jogging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1500);
+    }
+}
