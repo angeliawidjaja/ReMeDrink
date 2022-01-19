42a2efcf3a2fc9fcabe2e6245dd359826cb2765b
Angelia Widjaja <angeliawidjajaa@gmail.com>
2022-01-19 16:50:59 +0700

    feat: Handle Add Icons and Base RV List for Drink History

--- app/src/main/java/com/example/remedrink/app/landing/tracker/TrackerFragment.java
+++ app/src/main/java/com/example/remedrink/app/landing/tracker/TrackerFragment.java
@@ -24,13 +24,6 @@ public class TrackerFragment extends Fragment {
         binding = FragmentTrackerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textDashboard;
-//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
 
