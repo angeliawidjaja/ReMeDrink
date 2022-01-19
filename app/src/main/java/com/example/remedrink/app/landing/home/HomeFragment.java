69ee884d9178a5c2b49ff1a47ba4f0db64d63718
moninc0610 <morganmonica297@gmail.com>
2022-01-19 16:23:46 +0700

    input weight

--- app/src/main/java/com/example/remedrink/app/landing/home/HomeFragment.java
+++ app/src/main/java/com/example/remedrink/app/landing/home/HomeFragment.java
@@ -1,11 +1,19 @@
 package com.example.remedrink.app.landing.home;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +22,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.remedrink.PlankActivity;
+import com.example.remedrink.R;
 import com.example.remedrink.SquatActivity;
 import com.example.remedrink.app.landing.HomeActivity;
 import com.example.remedrink.databinding.FragmentHomeBinding;
@@ -36,9 +45,11 @@ public class HomeFragment extends Fragment {
         initListener();
 
         return root;
+
     }
 
     private void initListener() {
+
         binding.btnSquat.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), SquatActivity.class);
             startActivity(intent);
@@ -47,6 +58,42 @@ public class HomeFragment extends Fragment {
             Intent intent = new Intent(getActivity(), PlankActivity.class);
             startActivity(intent);
         });
+
+        binding.btnWeight.setOnClickListener(new View.OnClickListener() {
+            AlertDialog.Builder dialog;
+            LayoutInflater inflater;
+            View dialogView;
+
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                final View formsWeight = inflater.inflate(R.layout.form_weight,null, false);
+                final EditText inp_weight = (EditText) formsWeight.findViewById(R.id.inp_weight);
+
+                new AlertDialog.Builder(getActivity())
+                        .setView(formsWeight)
+                        .setPositiveButton("SET",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(
+                                            DialogInterface dialog, int id) {
+                                        String weight = "Weight : " + inp_weight.getText();
+                                        Toast.makeText(getActivity(), weight, Toast.LENGTH_SHORT).show();
+                                        dialog.cancel();
+                                    }
+                                })
+                        .setNegativeButton("CANCEL",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(
+                                            DialogInterface dialog, int id) {
+                                        dialog.dismiss();
+                                    }
+                        }).show();
+            }
+        });
+
     }
 
     @Override
